6caf1b7a3a16a99253b50809bbce6523d3c24d9d
XiongDuan <hgysdx@163.com>
2016-06-06 19:16:33 +0800

    change the make
    use the list.txt  instead of db_info.mat to make the train and val list

--- gen_caffe_list.m
+++ gen_caffe_list.m
@@ -1,14 +1,20 @@
 function gen_caffe_list(dbDirs, listpath)
     fid = fopen(listpath, 'w');
     for i = 1:length(dbDirs)
-        dbPath = fullfile(dbDirs{i},'db_info.mat');
-        imdb = load(dbPath);
-        imdb = imdb.db_info;
-        for j = 1:imdb.imnum
-%             imdb.path{j}(strfind(imdb.path{j}, ' ')) = '_';
-            I=imread(imdb.path{j});
-            fprintf('%s\n',imdb.path{j});
-            fprintf(fid, '%s %d\n', imdb.path{j}, imdb.label(j)-1 );
+        if exist(fullfile(dbDirs{i},'list.txt'), 'file')
+            dblist = importdata(fullfile(dbDirs{i},'list.txt'),'%s');
+            for j = 1:length(dblist)
+                fprintf('%s\n',dblist{j});
+                fprintf(fid, '%s\n', dblist{j});
+            end
+        else
+            dbPath = fullfile(dbDirs{i},'db_info.mat');
+            imdb = load(dbPath);
+            imdb = imdb.db_info;
+            for j = 1:imdb.imnum
+                fprintf('%s\n',imdb.path{j});
+                fprintf(fid, '%s %d\n', imdb.path{j}, imdb.label(j)-1 );
+            end
         end
     end
     fclose(fid);
